428d9c4d9ae2be98c351576671d73734e941efc5
František Šilhán <frantiseksilhann@gmail.com>
2023-10-09 13:27:47 +0000

    dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
+import {useState} from "react"
 import Greetings from "./components/Greetings"
 import Container from "./components/Container"
 import Counter from "./components/Counter"
 
 const App = () => {
+  const [a, setA] = useState<number>(0);
+  const [b, setB] = useState<number>(0);
 
   return (
     <>
       <Container number="2" text="t">AAAA<span>BBBB</span></Container>
       <Greetings text="Nazdar" />
       <Greetings text="hello" />
+      <Counter value={a} setValue={setA} />
       <Counter />
     </>
   )
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,14 @@
-import {useState} from "react"
 
-const Counter = () => {
-  const [value, setValue] = useState<number>(0);
+const Counter = (
+  {
+    value, setValue
+  }:
+  {
+    value: number, setValue: React.Dispatch<React.SetStateAction<number>>
+  }) => {
   return (
     <span>
+      <button onClick={() => setValue(0)}>Reset</button>
       <button onClick={() => setValue(prevValue => prevValue - 1)}>-</button>
       <span>{value}</span>
       <button onClick={() => setValue(prevValue => prevValue + 1)}>+</button>
